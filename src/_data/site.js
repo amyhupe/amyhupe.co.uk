63f6c529724de9c886ed36e4df928d0ae5d78541
Adam Silver <adambsilver@gmail.com>
2019-01-20 08:59:45 +0000

    Url

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
   "title": "Amy Hupe: description goes here",
   "description": "I'm Amy Hupe, my description goes here",
-  "url": process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:3000' : 'https://amyhupe.co.uk'
+  "url": process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:3000' : 'https://amy-hupe.netlify.com'
 };
