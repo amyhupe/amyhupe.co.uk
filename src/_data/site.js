7876936942d422623f85598a4da5b5b0a868f13c
Adam Silver <adambsilver@gmail.com>
2019-06-09 08:11:29 +0100

    Domain correction

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
   "title": "Amy Hupe: content designer at the Government Digital Service",
   "description": "I'm Amy Hupe and I care about making services accessible and inclusive, starting with the basics – the words we use to talk about them.",
-  "url": process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:3000' : 'https://amy-hupe.co.uk'
+  "url": process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:3000' : 'https://amyhupe.co.uk'
 };
