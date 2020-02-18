812494799ac923f35d0e1b35cea18f9b03c38afd
Andy Bell <me@andy-bell.co.uk>
2020-02-18 12:20:27 +0000

    Add weeknotes to RSS and site-wide links

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,15 @@ const MarkdownNunjucksExtension = require('nunjucks-markdown/lib/markdown_tag');
 const marked = require('marked');
 
 module.exports = eleventyConfig => {
-
   const cacheBusterOptions = {
     outputDirectory: 'dist'
   };
 
-  eleventyConfig.addNunjucksTag("markdown", function(nunjucksEngine) {
+  eleventyConfig.addCollection('rss', function(collection) {
+    return [...collection.getFilteredByGlob(`./src/articles/*.md`), ...collection.getFilteredByGlob(`./src/weeknotes/*.md`)].reverse();
+  });
+
+  eleventyConfig.addNunjucksTag('markdown', function(nunjucksEngine) {
     return new MarkdownNunjucksExtension(nunjucksEngine, marked);
   });
 
@@ -22,24 +25,19 @@ module.exports = eleventyConfig => {
     return str.replace(/\.html/g, '');
   });
 
-  eleventyConfig.addFilter("date", function(value) {
+  eleventyConfig.addFilter('date', function(value) {
     return moment(value).format('D MMM YYYY');
   });
 
-  eleventyConfig.addFilter("year", function(value) {
+  eleventyConfig.addFilter('year', function(value) {
     return moment(value).format('YYYY');
   });
 
   eleventyConfig.addFilter('strip_html', str => {
-    return str.replace(
-      /<script.*?<\/script>|<!--.*?-->|<style.*?<\/style>|<.*?>/g,
-      ''
-    )
+    return str.replace(/<script.*?<\/script>|<!--.*?-->|<style.*?<\/style>|<.*?>/g, '');
   });
 
-  eleventyConfig
-  .addPassthroughCopy('src/assets/fonts/')
-  .addPassthroughCopy('src/site.webmanifest')
+  eleventyConfig.addPassthroughCopy('src/assets/fonts/').addPassthroughCopy('src/site.webmanifest');
 
   return {
     templateFormats: ['njk', 'md', 'html', 'png', 'ico', 'svg', 'xml', 'jpg', 'jpeg', 'gif'],
@@ -53,5 +51,5 @@ module.exports = eleventyConfig => {
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     passthroughFileCopy: true
-  }
-}
+  };
+};
